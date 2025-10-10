a12cde0b2592047a87d554ba11373ef4a874ea9d
Manish Kumar <mankan2105@gmail.com>
2025-10-10 15:40:17 +0530

    initial commit

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop(i,a,b)     for(int i=(a); i<(b); i++)
+#define all(x)          (x).begin(), (x).end()
+
+void setupIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(int argc, char const *argv[]) {// setupIO();
+
+  cout <<" manish";
+
+    return 0;
+}
